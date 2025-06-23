da7aea9abfd2e03c002db489ed26de9cb107dbff
mardev60 <makil.uspn@gmail.com>
2025-06-23 11:25:36 +0200

    Creation de click service

--- internal/services/click_service.go
+++ internal/services/click_service.go
@@ -11,6 +11,11 @@ import (
 // ClickService est une structure qui fournit des méthodes pour la logique métier des clics.
 // Elle est juste composer de clickRepo qui est de type ClickRepository
 
+// ClickService est une structure qui fournit des méthodes pour la logique métier des clics.
+type ClickService struct {
+	clickRepo repository.ClickRepository
+}
+
 // NewClickService crée et retourne une nouvelle instance de ClickService.
 // C'est la fonction recommandée pour obtenir un service, assurant que toutes ses dépendances sont injectées.
 func NewClickService(clickRepo repository.ClickRepository) *ClickService {
@@ -22,14 +27,18 @@ func NewClickService(clickRepo repository.ClickRepository) *ClickService {
 // RecordClick enregistre un nouvel événement de clic dans la base de données.
 // Cette méthode est appelée par le worker asynchrone.
 func (s *ClickService) RecordClick(click *models.Click) error {
-	// TODO 1: Appeler le ClickRepository (CreateClick) pour créer l'enregistrement de clic.
-	// Gérer toute erreur provenant du repository.
-
+	if err := s.clickRepo.CreateClick(click); err != nil {
+		return fmt.Errorf("erreur lors de l'enregistrement du clic: %w", err)
+	}
+	return nil
 }
 
 // GetClicksCountByLinkID récupère le nombre total de clics pour un LinkID donné.
 // Cette méthode pourrait être utilisée par le LinkService pour les statistiques, ou directement par l'API stats.
 func (s *ClickService) GetClicksCountByLinkID(linkID uint) (int, error) {
-	// TODO 2: Appeler le ClickRepository (CountclicksByLinkID) pour compter les clics par LinkID.
-
+	count, err := s.clickRepo.CountClicksByLinkID(linkID)
+	if err != nil {
+		return 0, fmt.Errorf("erreur lors du comptage des clics: %w", err)
+	}
+	return count, nil
 }
