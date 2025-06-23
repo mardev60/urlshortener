d8444bb679e4fd31b9f354b4537f05adf64fea8e
mardev60 <makil.uspn@gmail.com>
2025-06-23 11:18:05 +0200

    Mise en place des models click et link

--- internal/models/link.go
+++ internal/models/link.go
@@ -1,9 +1,12 @@
 package models
 
-// TODO : Créer la struct Link
+import "time"
+
 // Link représente un lien raccourci dans la base de données.
 // Les tags `gorm:"..."` définissent comment GORM doit mapper cette structure à une table SQL.
-// ID qui est une primaryKey
-// Shortcode : doit être unique, indexé pour des recherches rapide (voir doc), taille max 10 caractères
-// LongURL : doit pas être null
-// CreateAt : Horodatage de la créatino du lien
+type Link struct {
+	ID        uint      `gorm:"primarykey"`
+	ShortCode string    `gorm:"uniqueIndex;size:10;not null"`
+	LongURL   string    `gorm:"not null"`
+	CreatedAt time.Time `gorm:"not null"`
+}
